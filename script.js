e837d70dc2c7ae0bb0be18c0a2db823144477993
EmilRemlo <disnox@icloud.com>
2024-09-11 10:59:57 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+let hinderArray = ["gul","blå","rød"];
+let hinder = document.getElementById("hinder");
+console.log(tilfeldigHinder());
+
+
+function tilfeldigHinder () {
+    let random = Math.floor(Math.random() * 3);
+    let hinderType = hinderArray[random];
+
+    if (hinderType = ("gul")) {
+    hinder.style.backgroundColor = 'yellow'
+    }
+    if (hinderType = ("rød")) {
+    hinder.style.backgroundColor = 'red'
+    }
+    if (hinderType = ("blå")) {
+    hinder.style.backgroundColor = 'blue'
+    }
+}
